65b9e8420e1a1ba3a9d8d0a25be5913d23151352
reza hutagaol <silentsound05@gmai.com>
2023-12-07 13:26:02 +0700

    add pojos account student

---
+++ src/main/java/com/reza/studentapplicantappapi/model/AccountStudent.java
@@ -0,0 +1,53 @@
+package com.reza.studentapplicantappapi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountStudent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String fullName;
+
+    @NotBlank
+    @Size(min = 4, max = 50)
+    private String username;
+
+    @Email
+    @Max(100)
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 50)
+    @Pattern(regexp = "^(?!.*\\s)(?=.*[A-Z])(?=.*\\W)(?!.*(?i:username|fullname))(.*)$")
+    private String password;
+
+    @NotBlank
+    @Size(min = 9, max = 16)
+    private String phone;
+
+    private Boolean isActive = false;
+    private Boolean isVerify = false;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
